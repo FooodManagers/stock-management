43051e38195743b837b4ed8d4e02f9c24d7aff8b
yutano <it232043@trident.ac.jp>
2024-12-09 22:52:32 +0900

    jancodeの読み取り結果の画面を作りました。(jancode読み取り画面は途中です)

---
+++ src/jancode/jan2.js
@@ -0,0 +1,27 @@
+const url = 'https://api.jancodelookup.com/?appId=e4c2c84cc860d50e4df967f1535645f3&query=4971749110373';
+const item = document.getElementById('result');
+const image = document.getElementById('image');
+fetch(url)
+    .then(responce => {
+        if (!responce.ok) {
+            throw new Error('ネットワークエラーが発生しました');
+        }
+        return responce.json();
+    })
+    .then((result) => {
+        Example(result);
+    })
+    .catch(error => {
+        console.error('処理中にエラーが発生しました', error);
+    });
+function Example(jsonObj) {
+    const data = jsonObj.product[0]
+    item.textContent = data.itemName;
+    // let result = [data.itemName, data.itemImageUrl];
+    // for (let i = 0; i < result.length; i++) {
+    //     let li = document.createElement('li');
+    //     li.textContent = result[i];
+    //     document.getElementById('canvas').appendChild(li);
+    // }
+    image.src = data.itemImageUrl;
+}
