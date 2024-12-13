56470a259eb9e5b226af3342a1f7a60bcf375335
Asura146 <kyo008g@gmail.com>
2024-12-13 11:42:40 +0900

    色を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        golden:'#FFD700',
+        orange:'#FFA500',
+        darkorange:'#FF8C00',
+        green:'#008000',
+        darkgreen:'#006400',
+      }
+    },
   },
   plugins: [],
 }
