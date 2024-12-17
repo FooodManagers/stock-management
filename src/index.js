12e69575900ee7011bb9ebc305bc7ce39efa7633
Asura146 <kyo008g@gmail.com>
2024-12-17 09:59:04 +0900

    NextUIを導入

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { NextUIProvider } from "@nextui-org/react";
+import React from "react";
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
+  </React.StrictMode>
+);
