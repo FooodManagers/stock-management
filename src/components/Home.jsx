327ce0058f955c601d146629c44e34f1c700e4f8
Asura146 <kyo008g@gmail.com>
2024-12-29 22:04:31 +0900

    余計なものを削除

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,31 +14,6 @@ export const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <Card className="max-w-[400px]">
-        <CardHeader className="flex gap-3">
-          <Image
-            alt="nextui logo"
-            height={40}
-            radius="sm"
-            src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-            width={40}
-          />
-          <div className="flex flex-col">
-            <p className="text-md">NextUI</p>
-            <p className="text-small text-default-500">nextui.org</p>
-          </div>
-        </CardHeader>
-        <Divider />
-        <CardBody>
-          <p>Make beautiful websites regardless of your design experience.</p>
-        </CardBody>
-        <Divider />
-        <CardFooter>
-          <Link isExternal showAnchorIcon href="https://github.com/nextui-org/nextui">
-            Visit source code on GitHub.
-          </Link>
-        </CardFooter>
-      </Card>
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y]}
         spaceBetween={50}
@@ -50,7 +25,7 @@ export const Home = () => {
         onSlideChange={() => console.log('slide change')}
       >
         <SwiperSlide>
-          <Card className="py-4">
+          <Card className="py-4 ">
             <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
               <p className="text-tiny uppercase font-bold">Daily Mix</p>
               <small className="text-default-500">12 Tracks</small>
