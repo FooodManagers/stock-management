3709fe7c05d2ac8ab962491a037d65133412c4e0
Asura146 <kyo008g@gmail.com>
2025-01-01 16:40:58 +0900

    フロントとバックをつなげた

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,21 +6,36 @@ import {List} from './components/List';
 import {Scan} from './components/Scan';
 import {Recipes} from './components/Recipes';
 import {Settings} from './components/Settings';
+import React,{useEffect,useState} from 'react';
 
 
 export const App = () => {
+  const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        fetch('/api/message')
+            .then((response) => response.json())
+            .then((data) => setMessage(data.message))
+            .catch((error) => console.error('Error:', error));
+    }, []);
   return (
-    <Router>
+    <>
       <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/list" element={<List />} />
-          <Route path="/scan" element={<Scan />} />
-          <Route path="/recipes" element={<Recipes />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
-        <BottomNaviBar />
+        <h1>{message}</h1>
       </div>
-    </Router>
+
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/list" element={<List />} />
+            <Route path="/scan" element={<Scan />} />
+            <Route path="/recipes" element={<Recipes />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+          <BottomNaviBar />
+        </div>
+      </Router>
+    </>
   );
 };
