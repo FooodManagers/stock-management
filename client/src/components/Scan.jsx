7e6c37af17127c82394b9a34b99232161eac8ccb
yutano <it232043@trident.ac.jp>
2025-01-02 19:47:52 +0900

    jancode内のデータの取得

--- client/src/components/Scan.jsx
+++ client/src/components/Scan.jsx
@@ -1,13 +1,15 @@
 import "../output.css"
 import "../scan.css"
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Quagga from 'quagga';
-import { createContext } from "react";
 import { useNavigate } from "react-router-dom";
 export const Scan = () => {
   const navigete = useNavigate();
   const [codes, setCodes] = useState([]);//jancode(20回分)が入る配列の宣言
-  const [detectedCode, setDetectedCode] = useState(null);
+  const [detectedCode, setDetectedCode] = useState(null); // 状態の初期化
+  let jancode;
+  const result = useRef();
+  const Jancode = useRef();
 
   const my_start = () => {//スキャンボタンを押したとき
     console.log("scannerスタート");
@@ -43,6 +45,10 @@ export const Scan = () => {
         if (newCodes.length === 20) {//jancode候補が20個集まったら
           Quagga.stop();//カメラをストップする
           sendCodesToServer(newCodes);
+          console.log("janCode_check:", detectedCode);
+          console.log("jancode:", jancode);
+          sendJanToServer(4902105280102);
+          //sendJanToServer(detectedCode);
         }
         return newCodes;
       });
@@ -61,7 +67,32 @@ export const Scan = () => {
       .then((data) => {
         if (data.success) {
           console.log("janCode:", data.adoptedValue);
+          jancode = data.adoptedValue;
           setDetectedCode(data.adoptedValue);
+        } else {
+          console.error("コード処理エラー");
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+  const sendJanToServer = (input) => {
+    fetch("http://localhost:3002/jancodefinish", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ data: input }),//javascriptオブジェクトをJSON形式の文字列に変換
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          console.log("check0");
+          const { item } = result;
+          console.log("check1");
+          item.textContent = data.itemName;
+          console.log("check2");
+          console.log("image:", data.itemImageUrl);
+
         } else {
           console.error("コード処理エラー");
         }
@@ -78,13 +109,17 @@ export const Scan = () => {
   }
   return (
     <div>
+      navigete('/Scanfinish');
       <div id="my_container">
         <div id="my_inner">
+          <input type="text" ref={result} defaultValue="" size="20" />
           <div>バーコードを映してください</div>
           <div>
             <button onClick={my_start}>スキャン</button>
           </div>
           <div id="my_quagga"></div>
+          <p>Detected Code: {detectedCode}</p>
+          <input ref={Jancode} defaultValue={4902105280102} />
           <div className="hand">
             <div id="triangle"></div>
             <button onClick={Hand}>手入力する</button>
--- client/src/jan.js
+++ client/src/jan.js
@@ -1,50 +1,44 @@
-const http = require('http');
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const fetch = require('node-fetch');
 
 const app = express();
 const PORT = 3002;
 
 //ミドルウェア
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 //jancodeを受け取る
-app.post('/jancodefinish', (req, res) => {
-    console.log("jancodefinish");
+app.post('/jancodefinish', async (req, res) => {
+    console.log("jancodefinish0");
     const data = req.body.data;
+    console.log("data=", data);
     const url = `https://api.jancodelookup.com/?appId=e4c2c84cc860d50e4df967f1535645f3&query=${data}`;
-    const item = document.getElementById('result');
-    const image = document.getElementById('image');
-    fetch(url)
-        .then(responce => {
-            if (!responce.ok) {
-                throw new Error('ネットワークエラーが発生しました');
-            }
-            return responce.json();
-        })
-        .then((result) => {
-            Example(result);
-        })
-        .catch(error => {
-            console.error('処理中にエラーが発生しました', error);
+    console.log("テスト前");
+    try {
+        const response = await fetch(url);
+        console.log("テスト");
+        if (!response.ok) {
+            throw new Error('ネットワークエラーが発生しました');
+        }
+        const result = await response.json();
+        console.log("データ");
+        const product = await result.product[0];
+        console.log("product.itemName", product.itemName);
+        console.log("product.itemImageUrl", product.itemImageUrl);
+        res.json({
+            succsess: true,
+            itemName: product.itemName,
+            itemImageUrl: product.itemImageUrl,
         });
-    function Example(jsonObj) {
-        const data = jsonObj.product[0]
-        item.textContent = data.itemName;
-        // let result = [data.itemName, data.itemImageUrl];
-        // for (let i = 0; i < result.length; i++) {
-        //     let li = document.createElement('li');
-        //     li.textContent = result[i];
-        //     document.getElementById('canvas').appendChild(li);
-        // }
-        image.src = data.itemImageUrl;
-        console.log('Adopted Value:', image.src);
-        res.json({ success: true, item, image });
+        console.log("データ完");
+
+    } catch (error) {
+        console.error('処理中にエラーが発生しました', error);
+        res.status(500).json({ success: false, error: error.message })
     }
 });
-
 //サーバ起動
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
