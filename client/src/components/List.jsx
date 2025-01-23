da0efe5345e729b2a1b70a43f92e6c77527fda40
Asura146 <147585329+Asura146@users.noreply.github.com>
2025-01-24 03:23:48 +0900

    買い物リストのUIを改善

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import "../output.css";
+import { Input, Button, Divider, Spacer, Card} from '@heroui/react';
 
 export const List = () => {
   const [items, setItems] = useState([]);
@@ -30,22 +31,32 @@ export const List = () => {
 
   return (
     <div>
-      <h1>買い物リスト</h1>
-      <input
-        type="text"
-        value={newItem}
-        onChange={(e) => setNewItem(e.target.value)}
-        placeholder="アイテムを入力"
-      />
-      <button onClick={handleAddItem}>追加</button>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={() => handleDeleteItem(index)}>削除</button>
-          </li>
-        ))}
-      </ul>
+      <Spacer y={2}/>
+      <h1 className='text-lg font-bold'>買い物リスト</h1>
+      <Divider className='my-4' />
+      <div className='flex w-full mx-auto md:flex-nowrap gap-4'>
+        <Input
+          type="text"
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          label="商品名"
+          fullWidth
+          className='mx-4'
+        />
+        <Button onPress={handleAddItem} className='my-auto mr-3'>追加</Button>
+      </div>
+      <Spacer y={4} />
+      {items.map((item, index) => (
+        <div key={index}>
+          <Card >
+            <div className='flex justify-between'>
+              <h1 className='text-lg font-bold my-auto mx-2'>{item}</h1>
+              <Button onPress={() => handleDeleteItem(index)} className='m-2'>削除</Button>
+            </div>
+          </Card>
+          <Spacer y={2} />
+        </div>
+      ))}
     </div>
   );
 };
